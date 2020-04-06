0ad91c54d224ce914a07cd9337ed91a55cc39b2a
Potty <nagypeti1998np@gmail.com>
2020-04-06 18:59:39 +0200

    firingModeAllDone

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -77,9 +77,7 @@ public class GT4500 implements SpaceShip {
         break;
 
       case ALL:
-        // try to fire both of the torpedo stores
-        //TODO implement feature
-
+        firingSuccess = primaryTorpedoStore.fire(1) || secondaryTorpedoStore.fire(1);
         break;
     }
 
